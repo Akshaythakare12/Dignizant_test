6c058d2fb118a27e8759a86a5507a0aeb42f2827
Akshaythakare12 <117450006+Akshaythakare12@users.noreply.github.com>
2022-12-28 19:56:28 +0530

    Add files via upload
    Dignizant_test Round

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoUri: process.env.MONGO_URI || "mongodb+srv://Akshay_admin:akki%40100796%23%24@cluster0.rwoq7mu.mongodb.net/?retryWrites=true&w=majority",
+    PORT: process.env.PORT || 3000,
+}
